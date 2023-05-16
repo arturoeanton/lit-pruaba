4b00c6730f750a75949d40cd54be9553819352aa
arturoeanton <arturoeanton@gmail.com>
2023-05-15 22:34:16 -0300

    commit

--- components/r2-card.js
+++ components/r2-card.js
@@ -1,131 +1,54 @@
-//componet lit
-import { LitElement, html, css  } from 'lit-element';
+import { LitElement, html, css } from 'lit';
 
 export class R2Card extends LitElement {
-    
-    static styles = css`
+  static styles = css`
     :host {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        -box-shadow: 0 0 20px #00000033;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
-        padding-left: 0px;
-        padding-right: 0px;
-    }
-
-    p {
-      margin-top: 8px;
+      display: block;
+      width: 250px;
+      padding: 16px;
+      border-radius: 8px;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      background-color: #2e2e2e;
+      color: #fff;
+      font-family: Arial, sans-serif;
     }
 
     .card-header {
-      text-align: left;
-      border-bottom: 1px solid #d885af;
-      padding: 6px;
-      cursor: pointer;
-      font-size: 18px;
-      color: #ffffff;
-    }
-
-    .card-content {
-      text-align: left;
-      padding: 3px;
-      margin-top: 0px;
-      margin-bottom: auto;
-      font-size: 16px;
-      color: #ffffff;
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 16px;
     }
 
     .card-footer {
-      border-top: 1px solid #d885af;
-      text-align: left;
-      padding: 3px;
-      font-size: 14px;
-      color: #ffffff;
+      margin-top: 16px;
+      text-align: right;
     }
 
-    .no-select {
-      -webkit-touch-callout: none;
-      -webkit-user-select: none;
-      -khtml-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
+    ::slotted(*) {
+      margin-bottom: 8px;
     }
   `;
 
   static properties = {
     title: { type: String },
-    color: { type: String },
-    width: { type: String },
-    height: { type: String },
-    padding: { type: String },
-    borderRadius: { type: String },
     footer: { type: String },
-    footer_hide: { type: Boolean },
-    footer_color: { type: String },
-    header_hide: { type: Boolean },
-    header_color: { type: String},
   };
 
   constructor() {
     super();
-    this.title = 'Title';
-    this.color = '#2a2b2f';
-    this.width = '250px';
-    this.height = '300px';
-    this.padding = '16px';
-    this.borderRadius = '9px';
-    this.footer = '.';
-    this.header_hide = false;
-    this.footer_hide = false;
-    this.footer_color = '#c74385';
-    this.header_color = '#c74385';
+    this.header = '';
+    this.footer = '';
   }
 
   render() {
     return html`
-        <style>
-          :host {
-            background-color: ${this.color};
-            width: ${this.width};
-            height: ${this.height};
-            border-radius: ${this.borderRadius};
-          }
-         
-          .header_color {
-            background-color: ${this.header_color};
-            border-radius: ${this.borderRadius} ${this.borderRadius} 0 0 ;
-          }
-
-          .header_color:hover {
-            outline: 1px solid blue;
-          }
-
-          .footer_color {
-            background-color: ${this.footer_color};
-            border-radius: 0 0 ${this.borderRadius} ${this.borderRadius} ;
-          }
-        </style>
-        ${this.header_hide ? '' : html`
-          <header class="card-header header_color no-select">
-            ${this.title}
-          </header>
-        `}
-
-        <div class="card-content">
-          <slot></slot>
-        </div>
-        
-        ${this.footer_hide ? '' : html`
-          <footer class="card-footer footer_color">
-            <p>${this.footer}</p>
-          </footer>
-        `}
-       
+      <div class="card-header">${this.title}</div>
+      <div class="card-content">
+        <slot></slot>
+      </div>
+      <div class="card-footer">${this.footer}         <slot name="footer"></slot></div>
     `;
   }
-   
 }
 
 customElements.define('r2-card', R2Card);
